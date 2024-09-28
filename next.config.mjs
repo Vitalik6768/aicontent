13806332babb48aa7e47f39961cd68602cedc26b
vitalik <vitalgreg1988@gmail.com>
2024-09-28 20:12:00 +0300

    new-test-vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
 
     images: {
       domains: ['img.clerk.com'], // Add any other domains if needed
     },
-
-    async headers() {
-      return [
-          {
-              // Apply headers to all routes
-              source: '/(.*)',
-              headers: [
-                  {
-                      key: 'Access-Control-Allow-Credentials',
-                      value: 'true',
-                  },
-                  {
-                      key: 'Access-Control-Allow-Origin',
-                      // Replace with your domain
-                      value: '*',
-                  },
-                  {
-                      key: 'Access-Control-Allow-Methods',
-                      value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                  },
-                  {
-                      key: 'Access-Control-Allow-Headers',
-                      value:
-                          
-'X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version',
-                  },
-              ],
-          },
-      ];
-  },
-
-
 
   };
   
