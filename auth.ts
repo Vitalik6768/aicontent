2692a5ea7455179193418cd13d1745efe361a52e
vitalik <vitalgreg1988@gmail.com>
2024-09-28 18:58:33 +0300

    fix-outh

--- auth.ts
+++ auth.ts
@@ -1,36 +1,36 @@
-import NextAuth, { User } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { getUserFromDb } from "./actions/user-actions"
-// Your own logic for dealing with plaintext password strings; be careful!
-// import { saltAndHashPassword } from "@/utils/password"
+// import NextAuth, { User } from "next-auth"
+// import Credentials from "next-auth/providers/credentials"
+// import { getUserFromDb } from "./actions/user-actions"
+// // Your own logic for dealing with plaintext password strings; be careful!
+// // import { saltAndHashPassword } from "@/utils/password"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        let user = null
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [
+//     Credentials({
+//       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+//       // e.g. domain, username, password, 2FA token, etc.
+//       credentials: {
+//         email: {},
+//         password: {},
+//       },
+//       authorize: async (credentials) => {
+//         let user = null
  
-        // logic to salt and hash password
-        // const pwHash = saltAndHashPassword(credentials.password)
+//         // logic to salt and hash password
+//         // const pwHash = saltAndHashPassword(credentials.password)
  
-        // logic to verify if the user exists
-        user = await getUserFromDb(credentials.email as string, credentials.password as string)
+//         // logic to verify if the user exists
+//         user = await getUserFromDb(credentials.email as string, credentials.password as string)
  
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
-          throw new Error("User not found.")
-        }
+//         if (!user) {
+//           // No user found, so this is their first attempt to login
+//           // meaning this is also the place you could do registration
+//           throw new Error("User not found.")
+//         }
  
-        // return user object with their profile data
-        return user.data as User
-      },
-    }),
-  ],
-})
+//         // return user object with their profile data
+//         return user.data as User
+//       },
+//     }),
+//   ],
+// })
