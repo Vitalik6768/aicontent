82c0170b15b92a10baee43a80ec48c72c47a2e5f
vitalik <vitalgreg1988@gmail.com>
2024-09-28 20:51:03 +0300

    test-8

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all API routes
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://aicontent-pi.vercel.app/", // Replace with your frontend URL
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          }
+        ]
+      }
+    ];
+  },
     images: {
       domains: ['img.clerk.com'], // Add any other domains if needed
     },
